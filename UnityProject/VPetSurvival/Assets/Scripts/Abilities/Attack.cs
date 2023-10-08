e807f011ca1068dc0dc1acaa00f0328bad88fd20
Bob Ebergen <bobebergen@gmail.com>
2023-10-08 18:43:42 +0200

    Start debuglogging chain of attack

--- UnityProject/VPetSurvival/Assets/Scripts/Abilities/Attack.cs
+++ UnityProject/VPetSurvival/Assets/Scripts/Abilities/Attack.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [CreateAssetMenu(menuName = "Abilities/Attacks/BasicAttack")]
 public class Attack : Ability
@@ -12,28 +13,36 @@ public class Attack : Ability
 
     public SphereCollider sphereCollider;
     public AttackHolder AttackHolder;
+
+    public bool ready = true;
+    private Transform origin;
     
     public void InitializeAttack(Transform _origin)
     {
         GameObject attackHolderObject = new GameObject($"Attack_{name}");
         attackHolderObject.transform.SetParent(_origin);
+        origin = _origin;
         AttackHolder = attackHolderObject.AddComponent<AttackHolder>();
-        sphereCollider = AttackHolder.AddComponent<SphereCollider>();
-        sphereCollider.radius = range;
-        sphereCollider.isTrigger = true;
         AttackHolder.OnEnterRange += addTarget;
         AttackHolder.OnExitRange += removeTarget;
     }
 
     private void addTarget(Transform _other)
     {
+        Debug.Log($"Adding Target {_other.name}");
         if (targets.Contains(_other))
         {
             return;
         }
         targets.Add(_other);
+        
+        Debug.Log($"attack ready: {ready}");
+        if (ready)
+        {
+            attack();
+        }
     }
-    
+
     private void removeTarget(Transform _other)
     {
         if (!targets.Contains(_other))
@@ -41,5 +50,44 @@ public class Attack : Ability
             return;
         }
         targets.Remove(_other);
+    }
+
+    private void attack()
+    {
+        int closestID = GetClosestTargetID(origin);
+        Transform closestTarget = targets[closestID];
+        
+        Debug.Log($"Attacking: {closestTarget.name}");
+        
+        //spawn projectile at origin
+
+        AttackHolder.StartCooldown(this);
+    }
+    
+    private int GetClosestTargetID(Transform _origin)
+    {
+        float smallestDistance = float.MaxValue;
+        int smallestDistanceID = -1;
+        for (int i = 0; i < targets.Count; i++)
+        {
+            float checkDistance = Vector3.Distance(targets[i].position, _origin.position);
+            if (smallestDistance > checkDistance)
+            {
+                smallestDistance = checkDistance;
+                smallestDistanceID = i;
+            }
+        }
+
+        return smallestDistanceID;
+    }
+
+    public IEnumerator StartCooldown()
+    {
+        Debug.Log("Cooldown Started");
+        ready = false;
+        yield return new WaitForSeconds(Cooldown);
+        ready = true;
+        Debug.Log("Attack Ready");
+
     }
 }
--- UnityProject/VPetSurvival/Assets/Scripts/Abilities/AttackHolder.cs
+++ UnityProject/VPetSurvival/Assets/Scripts/Abilities/AttackHolder.cs
@@ -1,18 +1,34 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackHolder : MonoBehaviour
 {
     public Action<Transform> OnEnterRange;
     public Action<Transform> OnExitRange;
-    
-    private void OnTriggerEnter(Collider other)
+
+    public void InitializeAttackHolder(Attack _attack)
     {
-        OnEnterRange?.Invoke(other.transform);
+        SphereCollider sphereCollider = this.AddComponent<SphereCollider>();
+        sphereCollider.radius = _attack.range;
+        sphereCollider.isTrigger = true;
+    }
+    
+    public void StartCooldown(Attack _attack)
+    {
+        Debug.Log("starting cooldown period");
+        StartCoroutine(_attack.StartCooldown());
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider _other)
     {
-        OnExitRange?.Invoke(other.transform);
+        Debug.Log($"target entered {_other.name}");
+        OnEnterRange?.Invoke(_other.transform);
+    }
+
+    private void OnTriggerExit(Collider _other)
+    {
+        Debug.Log($"target exited {_other.name}");
+        OnExitRange?.Invoke(_other.transform);
     }
 }
