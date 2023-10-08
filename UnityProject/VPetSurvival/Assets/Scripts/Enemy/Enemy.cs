fe93f517064665ef14bdf930f62c7d7d22130ae4
Mathijs van Nimwegen <mathijs.vannimwegen@gmail.com>
2023-10-08 17:35:07 +0200

    Now able to set angle and distance for player camera

--- UnityProject/VPetSurvival/Assets/Scripts/Enemy/Enemy.cs
+++ UnityProject/VPetSurvival/Assets/Scripts/Enemy/Enemy.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class Enemy : MonoBehaviour
 {
     public float Health = 1f;
     public float Damage = 1f;
     public float Speed = 1f;
     public float Presence = 1f;
+
+    private Rigidbody rb;
+    private BoxCollider bc;
+    private SpriteRenderer sr;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        bc = GetComponent<BoxCollider>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        bc.size = Vector3.zero;
+        bc.center = Vector3.zero;
+        bc.bounds.Encapsulate(sr.bounds);
+    }
 }
--- UnityProject/VPetSurvival/Assets/Scripts/PlayerCamera.cs
+++ UnityProject/VPetSurvival/Assets/Scripts/PlayerCamera.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public GameObject Player;
-    public Vector3 RelativePosition;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float DistanceFromPlayer = 5f;
+    public float Angle = 60f;
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        this.transform.position = Player.transform.position + RelativePosition;
+        float o = Mathf.Sin(Angle * Mathf.Deg2Rad) * DistanceFromPlayer;
+        float a = Mathf.Cos(Angle * Mathf.Deg2Rad) * DistanceFromPlayer;
+
+        this.transform.position = Player.transform.position + new Vector3(0f, o, -a);
+        this.transform.rotation = Quaternion.AngleAxis(Angle, Vector3.right);
     }
 }
