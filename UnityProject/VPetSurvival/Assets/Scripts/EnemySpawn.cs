4e3fb55c5fde93607c6eeb82620a1026e404bf1f
Mathijs van Nimwegen <mathijs.vannimwegen@gmail.com>
2023-10-08 15:45:29 +0200

    Enemies now spawn in a circle around the player

--- UnityProject/VPetSurvival/Assets/Scripts/EnemySpawn.cs
+++ UnityProject/VPetSurvival/Assets/Scripts/EnemySpawn.cs
@@ -1,13 +1,24 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawn : MonoBehaviour
 {
+    public GameObject Player;
+
 
     public float MaxPresence = 10f;
+    public float SpawnInterval = 1f;
+
+    public float SpawnRadius = 5f;
+
     public GameObject Enemy;
 
     private Transform enemyContainer;
+    private float gameTimeElapsed = 0f;
+    private float lastSpawnTime = 0f;
+    private bool spawning = false;
+
 
     private void Awake()
     {
@@ -22,7 +33,46 @@ public class EnemySpawn : MonoBehaviour
 
     private void Start()
     {
-        spawnEnemy(Enemy);
+        StartSpawning();
+    }
+
+    private void FixedUpdate()
+    {
+        doSpawning(Time.fixedDeltaTime);
+    }
+
+
+    public void StartSpawning()
+    {
+        spawning = true;
+    }
+
+    public void StopSpawning(bool _reset = false)
+    {
+        spawning = false;
+
+        if (_reset)
+        {
+            gameTimeElapsed = 0f;
+        }
+    }
+
+
+    private void doSpawning(float _deltaTime)
+    {
+        if (!spawning)
+        {
+            return;
+        }
+
+        gameTimeElapsed += _deltaTime;
+
+        float timeSinceLastSpawn = gameTimeElapsed - lastSpawnTime;
+        if (timeSinceLastSpawn >= SpawnInterval)
+        {
+            spawnEnemy(Enemy);
+            lastSpawnTime += SpawnInterval;
+        }
     }
 
     private void updatePresence()
@@ -35,10 +85,13 @@ public class EnemySpawn : MonoBehaviour
         GameObject newEnemy = GameObject.Instantiate(_enemy);
         newEnemy.transform.parent = enemyContainer;
         newEnemy.transform.position = getSpawnLocation();
-    } 
+    }
 
     private Vector3 getSpawnLocation()
     {
-        return enemyContainer.transform.position;
+        float randomAngle = Random.value * 360f;
+        Vector3 relativeSpawnVector = Quaternion.AngleAxis(randomAngle, Vector3.up) * Vector3.forward;
+        Vector3 playerPosition = Player.transform.position;
+        return playerPosition = relativeSpawnVector;
     }
 }
